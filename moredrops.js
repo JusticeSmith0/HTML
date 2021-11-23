1e2ed132547f4c3a7f9c929852b1806f81272f2c
Justice <jsmooth1142@gmail.com>
2021-11-23 14:06:10 -0500

    these are two piece of a website one part is html the other javascript

---
+++ moredrops.js
@@ -0,0 +1,9 @@
+let newestHotel = {
+    guestFirstName:"",
+    guestLastName: "",
+    guestStayLength:9999,
+    guestRoomOptions:["Single","Double","Queen"],
+    availbleRooms:[[100,101,105],[203,206,208,210],[300,301,302]],
+    bookedRooms:[[],[],[]],
+    guestRoomType:"",
+}
